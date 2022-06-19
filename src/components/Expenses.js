4ace93b4e637d34b0dafe6a67a13a5b819024dfc
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-19 13:47:16 -0400

    created Card and Card css files to house re-useable styles for components

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card'
 import './Expenses.css'
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {
         props.items.map(ex => {
-            return <ExpenseItem key={ex.id} title={ex.title} amount={ex.amount} date={ex.date} />
+            return <ExpenseItem id={ex.id} title={ex.title} amount={ex.amount} date={ex.date} />
         })
       }
-    </div>
+    </Card>
   );
 }
 
