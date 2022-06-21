09a3075c55fc856e2d4b914ec64ef398306003b8
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-21 11:54:11 -0400

    wip

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css'
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
      //create these variables instead of coding them into the divs to make them reusable
      const month = props.date.toLocaleString('en-US', { month: 'long'})
      const day = props.date.toLocaleString('en-US', {day: '2-digit'})
