a3b5e88cea0b836ec6332cfd67b790c09896a6fc
EstrellaDionis <EstrellaDionis@gmail.com>
2022-10-31 12:55:22 -0400

    update comment on Card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,8 @@
 import './Card.css'
 
 const Card = (props) => {
+    // The purpose of this component is to remove duplicated code for styling. The duplicated code is in Card.css but, so that other components can use this, we use the Card component
+    // and the classes here is saying 'card ' as the name AND and whatever other className when we pass this as a prop. This way, it's keeping the other styling as well as this
     const classes = 'card ' + props.className
 
     return <div className={classes}>{props.children}</div>
